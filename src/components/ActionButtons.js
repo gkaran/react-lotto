6438ab1088b18e3365e600bd6e8a888a8740ff64
George Karanikas <gkaran89@gmail.com>
2021-07-07 08:18:53 +0300

    (fix) use fragment for actionbuttons instead of list

--- src/components/ActionButtons.js
+++ src/components/ActionButtons.js
@@ -7,13 +7,13 @@ const ActionButtons = observer(({game}) => {
     const onGameQuickPick = () => game.quickPickAll();
     const onGameReset = () => game.reset();
 
-    return [
-        <button className='border-white text-white border-2 rounded-lg p-2 text-lg mr-2' onClick={onTabClear}>CLR</button>,
-        <button className='border-white text-white border-2 rounded-lg p-2 text-lg mr-2' onClick={onGameClear}>CLR ALL</button>,
-        <button className='border-white text-white border-2 rounded-lg p-2 text-lg mr-2' onClick={onTabQuickPick}>QS</button>,
-        <button className='border-white text-white border-2 rounded-lg p-2 text-lg mr-2' onClick={onGameQuickPick}>QS ALL</button>,
+    return (<>
+        <button className='border-white text-white border-2 rounded-lg p-2 text-lg mr-2' onClick={onTabClear}>CLR</button>
+        <button className='border-white text-white border-2 rounded-lg p-2 text-lg mr-2' onClick={onGameClear}>CLR ALL</button>
+        <button className='border-white text-white border-2 rounded-lg p-2 text-lg mr-2' onClick={onTabQuickPick}>QS</button>
+        <button className='border-white text-white border-2 rounded-lg p-2 text-lg mr-2' onClick={onGameQuickPick}>QS ALL</button>
         <button className='border-white text-white border-2 rounded-lg p-2 text-lg' onClick={onGameReset}>R</button>
-    ];
+    </>);
 });
 
 export default ActionButtons;
